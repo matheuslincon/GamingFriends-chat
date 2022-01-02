3d04f2b7766df7bce1e079fd80ba8ab9e441d792
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-02 11:08:00 -0300

    implemented logout function

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,10 +3,12 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-import ConsoleIcon from '../assets/console.png'
-import LogoutIcon from '../assets/logout.png'
+import ConsoleIcon from '../assets/console.png';
+import LogoutIcon from '../assets/logout.png';
 
-const SideBar = () => {
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => {
   return (
     <div className="channel-list__sidebar">
       <div className="channel-list__sidebar__icon1">
@@ -15,7 +17,7 @@ const SideBar = () => {
         </div>
       </div>
       <div className="channel-list__sidebar__icon2">
-        <div className="icon1__inner">
+        <div className="icon1__inner" onClick={logout}>
           <img src={LogoutIcon} alt="Logout" width="30" />
         </div>
       </div>
@@ -32,9 +34,22 @@ const GamingHeader = () => {
 }
 
 const ChannelListContainer = () => {
+
+  const logout = () => {
+    cookies.remove('token');
+    cookies.remove('userId');
+    cookies.remove('username');
+    cookies.remove('fullName');
+    cookies.remove('avatarURL');
+    cookies.remove('hashedPassword');
+    cookies.remove('phoneNumber');
+
+    window.location.reload();
+  };
+
   return (
     <>
-      <SideBar/>
+      <SideBar logout={logout}/>
       <div className="channel-list__list__wrapper">
         <GamingHeader/>
         <ChannelSearch/>
