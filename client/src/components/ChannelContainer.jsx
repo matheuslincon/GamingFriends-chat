19b0474ccde8785a9d28419312d44b9fe087249c
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-04 23:10:27 -0300

    small fixes

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Channel, useChatContext, MessageTeam } from 'stream-chat-react';
+import { Channel, MessageTeam } from 'stream-chat-react';
 
 import { ChannelInner, CreateChannel, EditChannel } from './';
 
 const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
-  const { channel } = useChatContext();
 
   if(isCreating) {
     return(
